17c3c1c88a75ba43bba136a0a56214e04a3d2451
CarrotLN <gerelikes@hotmail.com>
2020-02-04 01:16:20 +0700

    change database

--- condb.php
+++ condb.php
@@ -1,5 +1,5 @@
 <?php
-$con= mysqli_connect("localhost","root","","project_service") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("us-cdbr-iron-east-04.cleardb.net","b2b2a61ce15814","ed632b23","heroku_b6a18ce63faf858") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE );
 date_default_timezone_set('Asia/Bangkok');
